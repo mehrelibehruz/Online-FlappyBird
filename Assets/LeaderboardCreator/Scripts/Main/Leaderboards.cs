fe9df85219130e46cf7377ad4b99d4848849c992
mehrelibehruz <mehrelibehruz@gmail.com>
2023-11-25 08:57:09 +0400

    First Commit

---
+++ Assets/LeaderboardCreator/Scripts/Main/Leaderboards.cs
@@ -0,0 +1,8 @@
+namespace Dan.Main
+{
+    public static class Leaderboards
+    {
+        public static LeaderboardReference DemoSceneLeaderboard = new LeaderboardReference("980324f10e2f03fc0fd6a38a70f88123919a6aed35a4aea86211ad58b68db9c4");
+        public static LeaderboardReference FlappyBirdOnline = new LeaderboardReference("7852a82dd1fa0fbbececc747eb02ec4a4deacf7763e013fbbedb5b41cb10dd7b");
+    }
+}
